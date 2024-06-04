e6ef3edade6a66a2ca452bbef97bba3030262d73
Junsoo Park <matthiaspark0514@gmail.com>
2024-06-04 13:33:11 -0700

    Feat: Server Actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,4 @@
-'use server';
-
-import * as auth from '@/auth';
-
-export async function signIn() {
-    return auth.signIn('linkedin');
-}
-
-export async function signOut() {
-    return auth.signOut();
-}
+export {signIn} from './sign-in';
+export {signOut} from './sign-out';
+export {createComment} from './create-comment';
+export {createDemon} from './create-demon';
