04ab2dd4391613eeb6fdc3e0dcc76adc3e31e198
Junsoo Park <matthiaspark0514@gmail.com>
2024-06-10 16:02:44 -0700

    Feat: Create Demonds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+import DemonCreateForm from "@/components/demons/demon-create-form"
+
 export default function Home() {
 
-  return <div>Home Page</div>
+  return <div className="grid grid-cols-4 gap-4 p-4">
+    <div className="col-span-3">
+      <h1 className="text-xl m-2">Top Demonds</h1>
+    </div>
+    <div>
+      <DemonCreateForm />
+    </div>
+  </div>
 }
---
+++ src/components/demons/demon-create-form.tsx
@@ -0,0 +1,3 @@
+export default function DemonCreateForm() {
+    return <div> Demon Create Form</div>
+}
